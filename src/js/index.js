e9b960183259326a06c3793faec9e13d887a36e4
vMaxiMv <maximvasiliyev314@gmail.com>
2025-05-05 23:45:25 +0300

    rr-913: add api query and parsing vacancies response

--- src/js/index.js
+++ src/js/index.js
@@ -1,158 +1,92 @@
-// Работа с массивами
+async function getVacanciesQuery() {
+    try {
+    const response = await fetch(`https://api.hh.ru/vacancies`)
+    const data = await response.json()
+    const filteredVacancies = 
+        data.items.map(item => ({
+            id: item.id,
+            name: item.name,
+            area: item.area.name,
+            schedule: item.employment.name,
+            employer: item.employer,
+            snippet: item.snippet.requirement
+        }))
+        return  filteredVacancies  
+    }
+    catch(err) {
+        console.log('Error', err)
+    }
+}
 
-//const arr = ['banana', true, 1, 'car', {}, { a: 1 }, 5, true, true, false, 455, {}]
+function generateVacancyCard(vacancy, index) {
+    const logoUrl = vacancy.employer.logo_urls && vacancy.employer.logo_urls.original
+    const companyName = vacancy.employer.name
+    const web = vacancy.employer.alternate_url
 
-// 1. Написать функцию, которая вернет новый массив, где все числа массива arr будут увеличины в 2 раза
+    return `
+    <li class="vacancies__item">
+            <article class="vacancies-card">
+                <div class="vacancies-card__header">
+                    <div class="vacancies-card__title-info">
+                        <h3 class="vacancies-card__title">${vacancy.name}</h3>
+                        <img class="vacancies-card__logo" src=${logoUrl} alt="${companyName} logo"/>
+                    </div>
+                    <button class="vacancies-card__respond">Respond</button>
+                </div>
+                <dl class="vacancies-card__info">
+                    <div class="vacancies-card__item">
+                        <dt class="vacancies-card__label">Form</dt>
+                        <dd class="vacancies-card__value">${vacancy.schedule}</dd>
+                    </div>
+                    <div class="vacancies-card__item">
+                        <dt class="vacancies-card__label">Company</dt>
+                        <dd class="vacancies-card__value">${companyName}</dd>
+                    </div>
+                    <div class="vacancies-card__item">
+                        <dt class="vacancies-card__label">Web</dt>
+                        <dd class="vacancies-card__value">${web}</dd>
+                    </div>
+                    <div class="vacancies-card__item">
+                        <dt class="vacancies-card__label">Address</dt>
+                        <dd class="vacancies-card__value">${vacancy.area}</dd>
+                    </div>
+                </dl>
+                <div class="vacancies-card__description-section">
+                    <p class="vacancies-card__desc">${vacancy.snippet}</p>
+                    <div class="toggle-list">
+                        <input type="checkbox" id="toggle-details-${index}" class="toggle-list__checkbox" hidden>
+                        <p class="toggle-list__snapshot-title">Success Snapshot</p>
+                        <ul class="toggle-list__list">
+                            <li class="toggle-list__item toggle-list__item--blur">Develop solutions for product offerings using relevant technologies;</li>
+                            <li class="toggle-list__item toggle-list__item--hidden">Work closely with a Team Lead and a UI team to deliver stable, efficient, and scalable solutions;</li>
+                            <li class="toggle-list__item toggle-list__item--hidden">Be proud of stable, reliable, and secure code.</li>
+                        </ul>
+                        <label for="toggle-details-${index}" class="toggle-list__btn"></label>
+                    </div>
+                </div>
+            </article>
+        </li>
+    `
+}
 
-// function func (arr) {
-//     return arr.map(item => {
-//        return typeof(item)==='number' ?
-//        item * 2 :
-//        item;
-//     })
-// }
-// const newArr = func(arr)
-// console.log(newArr)
+function renderVacanciesList(vacancies, container){
+    const vacanciesHtml = vacancies.map((vacancy, index)=>generateVacancyCard(vacancy, index + 1))
+    container.innerHTML = vacanciesHtml
+}
 
-// 2. Написать новую функцию, которая вернет объект, в котором будут поля с названиями равными типам данным, а в полях будет итоговый подсчет кол-ва элементов в массиве arr
 
-// function func (arr){
-//     const obj = arr.map(item=>typeof(item)).reduce((acc, item)=>{
-//         acc[item] = (acc[item] || 0) + 1
-//         return acc
-//     },{})
-//     return obj
-// }
-// const obj = func(arr)
-// console.log(obj)
+async function renderPage(){
+    const vacanciesList = document.querySelector('.vacancies ul')
+    const vacancies = await getVacanciesQuery()
+    
+    if(vacancies && vacancies.length > 0){
+        renderVacanciesList(vacancies, vacanciesList)
+    }
+    else{
+        vacanciesList.innerHTML = `<li>Вакансии не найдены</li>`
+    }
+}
 
-// 3. Написать функцию, которая отсортирует массив следующим образом: булевы, числа, строки, объекты
-
-// function func (arr){
-//    return arr.sort((a,b)=>{
-//        const types = ['boolean', 'number', 'string', 'object']
-//        return types.indexOf(typeof(a)) - types.indexOf(typeof(b)) 
-//     })
-// }
-// const newArr = func(arr)
-// console.log(newArr)
-
-// 4. Написать функцию, которая принимает неограниченное число параметров и возвращает массив, в котором каждое число умножается на общее количество параметров.
-
-// function func(...args){
-//     const argsCount = args.length
-//     return args.map(item=>{
-//         return item * argsCount
-//     })
-// }
-
-// const newArr = func(3,4,5,6,10)
-// console.log(newArr)
-
-// 5.Написать функцию, которая принимает массив слов, а выводит массив уникальных слов отсортированных по количеству повторов этого слова,
-//  если у 2х слов параметр уникальности одинаковый, то они должны быть отсортированы между собой по алфавиту.
-
-// function func (arr){
-//     const obj = arr.reduce((acc, item)=>{
-//          acc[item] = (acc[item] || 0) + 1
-//         return acc
-//      },{})
-//     return Object.entries(obj)
-//      .sort(([aWord, aCount],[bWord, bCount])=>{
-//        return bCount - aCount || aWord.localeCompare(bWord) 
-//      })
-//      .map(([word]) => word)
-// }
-
-// const newArr = func(['fruit', 'keyboard', 'word', 'word', 'keyboard', 'word', 'fruit', 'banana'])
-// console.log(newArr)
-
-// Работа с объектами
-
-// 1. Написать функцию, которая посчитает количество ключей
-
-// const obj = {
-//     a: 1,
-//     b: 2,
-//     c: 3,
-//     d: 1
-// }
-
-// function func(obj){
-//    return Object.keys(obj).length
-// }
-// const qty = func(obj)
-// console.log(qty)
-
-// 2. Написать функцию, которая конвертнет исходный объект в новый по структуре ниже
-
-// const obj = {
-//         a: 1,
-//         b: 2,
-//         c: 3
-// }
-
-// function func (obj){
-//     // Object.entries(obj).map(([key, value])=>{
-//     //     key === 'a' && key === 'c' && 
-//     // })
-//     return convertedObj = {...obj, d: obj.a + obj.c}
-// }
-// const newObj = func(obj);
-// console.log(newObj)
-
-// Работа с классами
-
-// 1. Создать AreaCalculator класс, который будет считать площади фигур. Дополнительно создать 2 класса: Square и Circle.
-
-// class AreaCalculator {
-
-// }
-
-// class Square {
-//     constructor(side) {
-//       this.side = side;
-//     }
-//   area(){
-//     return this.side ** 2
-//   }
-//   }
-// const squareArea = new Square(5).area()
-// console.log(squareArea)
-
-// class Circle {
-//     constructor(radius) {
-//       this.radius = radius;
-//     }
-//   area(){
-//     return Math.PI * (this.radius ** 2)
-//   }
-//   }
-// const circleArea = new Circle(10).area()
-// console.log(circleArea)
-
-// class Triangle{
-//     constructor(side, height) {
-//       this.side = side;
-//       this.height = height
-//     }
-//   area(){
-//     return (this.side * this.height) / 2
-//   }
-//   }
-
-//   const triangleArea = new Triangle(10, 8).area()
-//   console.log(triangleArea)
-
-// class AreaCalculator {
-//     constructor(shapes){
-//         this.shapes = shapes
-//     }
-//     sum(){
-//        return this.shapes.reduce((acc, item)=>{
-//             return acc = acc + item.area()
-//         }, 0)
-//     }
-// }
-
-// console.log(new AreaCalculator([new Square(5), new Circle(10), new Triangle(10, 8)]).sum())
+window.addEventListener('DOMContentLoaded', () => {
+    renderPage()
+})
