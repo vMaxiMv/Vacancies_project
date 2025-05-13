1783b36a25441eba0261e72b579fa6a2946a58ac
vMaxiMv <maximvasiliyev314@gmail.com>
2025-05-13 11:02:58 +0300

    rr-913: create api directory for query function

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,5 @@
-async function getVacanciesQuery(page) {
-    try {
-    const response = await fetch(`https://api.hh.ru/vacancies?page=${page}`)
-    const data = await response.json()
-    const filteredVacancies = 
-        data.items.map(item => ({
-            id: item.id,
-            name: item.name,
-            area: item.area.name,
-            schedule: item.employment.name,
-            employer: item.employer,
-            snippet: item.snippet.requirement
-        }))
-        return  filteredVacancies  
-    }
-    catch(err) {
-        console.log('Error', err)
-    }
-}
+import getVacanciesQuery from "./api/vacanciesQuery.js"
+
 
 function generateVacancyCard(vacancy, index) {
     const logoUrl = vacancy.employer.logo_urls && vacancy.employer.logo_urls.original
